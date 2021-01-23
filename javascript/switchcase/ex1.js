908ad0744d0c793c8df02bb7b61625c99d390e96
chiru <chirukosanam123@gmail.com>
2021-01-23 14:02:48 +0530

    commits to github

---
+++ javascript/switchcase/ex1.js
@@ -0,0 +1,11 @@
+let msg='gm';
+switch(msg){
+    case 'gm':
+        console.log("good morning");
+    case 'ga':
+        console.log("good afternoon");    
+    case 'gn':
+        console.log("good night");
+    default:
+        console.log("check your msg");
+}
---
+++ javascript/switchcase/ex2.js
@@ -0,0 +1,15 @@
+let msg='gm';
+switch(msg){
+    case 'gm':
+        console.log("good morning");
+        break;
+    case 'ga':
+        console.log("good afternoon");
+        break;
+    case 'gn':
+        console.log("good night");
+        break;
+    default:
+        console.log("check your msg");
+        break;
+}
