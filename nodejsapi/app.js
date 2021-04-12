88e0165a41ff86b2f975203da78099a4481d44fb
chiru381 <chirukosanam123@gmail.com>
2021-04-12 12:32:40 +0530

    api

--- nodejsapi/app.js
+++ nodejsapi/app.js
@@ -1,24 +1,23 @@
-//create express server
 import express from 'express';
-import path from 'path';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import path from 'path';
 
-dotenv.config({path:"./config/config.env"});
+dotenv.config({path: "./config.config.env"});
 const app=express();
-app.get('/',(req,res)=>{
+app.get('/', (req,res)=>{
     res.send("Hello, This is express server");
 });
-app.get('/employee',(req,res)=>{
-    fs.readFile(path.join(__dirname,"employee", "data", "employee.json"), "utf-8", (err, data)=>{
+app.get('/employee', (req,res)=>{
+    fs.readFile(path.join(__dirname, "employee","data","employee.json"), "utf-8", (err,data)=>{
         if(err) throw err;
         res.status(200).json(JSON.parse(data));
     })
 });
-app.get('/contacts', (req, res)=>{
-    res.send("Contacts Data");
+app.get('contacts',(req,res)=>{
+    res.send("contacts data");
 });
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
-    console.log(`Server is running on Port Number: ${process.env.PORT}`);
+    console.log(`Server is running on Port Number:${process.env.PORT}`);
 });
